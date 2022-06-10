2693749443bfd1b30eeb62e45dde030b97214886
Jacob Riddle <jriddle@linode.com>
2022-06-10 15:32:20 +0000

    postgres tests passing, generating other db fixtures

--- test/integration/postgres_test.go
+++ test/integration/postgres_test.go
@@ -81,6 +81,8 @@ func TestDatabase_Postgres_Suite(t *testing.T) {
 		t.Errorf("failed to update db %d: %v", database.ID, err)
 	}
 
+	waitForDatabaseUpdated(t, client, db.ID, linodego.DatabaseEngineTypePostgres, db.Created)
+
 	if db.ID != database.ID {
 		t.Errorf("updated db does not match original id")
 	}
