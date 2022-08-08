a7b0508e7047a113c319fc25bd5eca3ad474e3ba
Jacob Riddle <jriddle@linode.com>
2022-08-08 19:40:24 +0000

    merging all the list helper functions into one

--- pagination.go
+++ pagination.go
@@ -6,8 +6,6 @@ package linodego
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"strconv"
 
 	"github.com/go-resty/resty/v2"
@@ -50,7 +48,7 @@ func applyListOptionsToRequest(opts *ListOptions, req *resty.Request) {
 }
 
 type PagedResponse interface {
-	endpoint(*Client) string
+	endpoint(*Client, ...interface{}) string
 	castResult(*resty.Request, string) (int, int, error)
 }
 
@@ -59,12 +57,12 @@ type PagedResponse interface {
 // When opts (or opts.Page) is nil, all pages will be fetched and
 // returned in a single (endpoint-specific)PagedResponse
 // opts.results and opts.pages will be updated from the API response
-// nolint
-func (c *Client) listHelper(ctx context.Context, pager PagedResponse, opts *ListOptions) error {
+func (c *Client) listHelper(ctx context.Context, pager PagedResponse, opts *ListOptions,
+	ids ...interface{}) error {
 	req := c.R(ctx)
 	applyListOptionsToRequest(opts, req)
 
-	pages, results, err := pager.castResult(req, pager.endpoint(c))
+	pages, results, err := pager.castResult(req, pager.endpoint(c, ids...))
 	if err != nil {
 		return err
 	}
@@ -75,7 +73,7 @@ func (c *Client) listHelper(ctx context.Context, pager PagedResponse, opts *List
 		if opts.Page == 0 {
 			for page := 2; page <= pages; page++ {
 				opts.Page = page
-				if err := c.listHelper(ctx, pager, opts); err != nil {
+				if err := c.listHelper(ctx, pager, opts, ids...); err != nil {
 					return err
 				}
 			}
@@ -85,7 +83,7 @@ func (c *Client) listHelper(ctx context.Context, pager PagedResponse, opts *List
 	}
 	for page := 2; page <= pages; page++ {
 		newOpts := ListOptions{PageOptions: &PageOptions{Page: page}}
-		if err := c.listHelper(ctx, pager, &newOpts); err != nil {
+		if err := c.listHelper(ctx, pager, &newOpts, ids...); err != nil {
 			return err
 		}
 	}
@@ -93,214 +91,23 @@ func (c *Client) listHelper(ctx context.Context, pager PagedResponse, opts *List
 	return nil
 }
 
-// listHelperWithID abstracts fetching and pagination for GET endpoints that
-// require an Id (second level endpoints).
-// When opts (or opts.Page) is nil, all pages will be fetched and
-// returned in a single (endpoint-specific)PagedResponse
-// opts.results and opts.pages will be updated from the API response
-// nolint
-func (c *Client) listHelperWithID(ctx context.Context, i interface{}, idRaw interface{}, opts *ListOptions) error {
-	var (
-		err     error
-		pages   int
-		results int
-		r       *resty.Response
-	)
-
-	req := c.R(ctx)
-	applyListOptionsToRequest(opts, req)
-
-	id, _ := idRaw.(int)
-
-	switch v := i.(type) {
-	case *DomainRecordsPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(DomainRecordsPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			response, ok := r.Result().(*DomainRecordsPagedResponse)
-			if !ok {
-				return fmt.Errorf("response is not a *DomainRecordsPagedResponse")
-			}
-			pages = response.Pages
-			results = response.Results
-			v.appendData(response)
-		}
-	case *FirewallDevicesPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(FirewallDevicesPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*FirewallDevicesPagedResponse).Pages
-			results = r.Result().(*FirewallDevicesPagedResponse).Results
-			v.appendData(r.Result().(*FirewallDevicesPagedResponse))
-		}
-	case *InstanceConfigsPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(InstanceConfigsPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*InstanceConfigsPagedResponse).Pages
-			results = r.Result().(*InstanceConfigsPagedResponse).Results
-			v.appendData(r.Result().(*InstanceConfigsPagedResponse))
-		}
-	case *InstanceDisksPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(InstanceDisksPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*InstanceDisksPagedResponse).Pages
-			results = r.Result().(*InstanceDisksPagedResponse).Results
-			v.appendData(r.Result().(*InstanceDisksPagedResponse))
-		}
-	case *InstanceVolumesPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(InstanceVolumesPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*InstanceVolumesPagedResponse).Pages
-			results = r.Result().(*InstanceVolumesPagedResponse).Results
-			v.appendData(r.Result().(*InstanceVolumesPagedResponse))
-		}
-	case *InvoiceItemsPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(InvoiceItemsPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*InvoiceItemsPagedResponse).Pages
-			results = r.Result().(*InvoiceItemsPagedResponse).Results
-			v.appendData(r.Result().(*InvoiceItemsPagedResponse))
-		}
-	case *LKEClusterAPIEndpointsPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(LKEClusterAPIEndpointsPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*LKEClusterAPIEndpointsPagedResponse).Pages
-			results = r.Result().(*LKEClusterAPIEndpointsPagedResponse).Results
-			v.appendData(r.Result().(*LKEClusterAPIEndpointsPagedResponse))
-		}
-	case *LKENodePoolsPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(LKENodePoolsPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*LKENodePoolsPagedResponse).Pages
-			results = r.Result().(*LKENodePoolsPagedResponse).Results
-			v.appendData(r.Result().(*LKENodePoolsPagedResponse))
-		}
-	case *MySQLDatabaseBackupsPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(MySQLDatabaseBackupsPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*MySQLDatabaseBackupsPagedResponse).Pages
-			results = r.Result().(*MySQLDatabaseBackupsPagedResponse).Results
-			v.appendData(r.Result().(*MySQLDatabaseBackupsPagedResponse))
-		}
-	case *MongoDatabaseBackupsPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(MongoDatabaseBackupsPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*MongoDatabaseBackupsPagedResponse).Pages
-			results = r.Result().(*MongoDatabaseBackupsPagedResponse).Results
-			v.appendData(r.Result().(*MongoDatabaseBackupsPagedResponse))
-		}
-	case *PostgresDatabaseBackupsPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(PostgresDatabaseBackupsPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*PostgresDatabaseBackupsPagedResponse).Pages
-			results = r.Result().(*PostgresDatabaseBackupsPagedResponse).Results
-			v.appendData(r.Result().(*PostgresDatabaseBackupsPagedResponse))
-		}
-	case *NodeBalancerConfigsPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(NodeBalancerConfigsPagedResponse{}).Get(v.endpointWithID(c, id))); err == nil {
-			pages = r.Result().(*NodeBalancerConfigsPagedResponse).Pages
-			results = r.Result().(*NodeBalancerConfigsPagedResponse).Results
-			v.appendData(r.Result().(*NodeBalancerConfigsPagedResponse))
-		}
-	case *TaggedObjectsPagedResponse:
-		idStr := idRaw.(string)
-
-		if r, err = coupleAPIErrors(req.SetResult(TaggedObjectsPagedResponse{}).Get(v.endpointWithID(c, idStr))); err == nil {
-			pages = r.Result().(*TaggedObjectsPagedResponse).Pages
-			results = r.Result().(*TaggedObjectsPagedResponse).Results
-			v.appendData(r.Result().(*TaggedObjectsPagedResponse))
-		}
-	/**
-	case TicketAttachmentsPagedResponse:
-		if r, err = req.SetResult(v).Get(v.endpoint(c)); r.Error() != nil {
-			return NewError(r)
-		} else if err == nil {
-			pages = r.Result().(*TicketAttachmentsPagedResponse).Pages
-			results = r.Result().(*TicketAttachmentsPagedResponse).Results
-			v.appendData(r.Result().(*TicketAttachmentsPagedResponse))
-		}
-	case TicketRepliesPagedResponse:
-		if r, err = req.SetResult(v).Get(v.endpoint(c)); r.Error() != nil {
-			return NewError(r)
-		} else if err == nil {
-			pages = r.Result().(*TicketRepliesPagedResponse).Pages
-			results = r.Result().(*TicketRepliesPagedResponse).Results
-			v.appendData(r.Result().(*TicketRepliesPagedResponse))
-		}
-	**/
-	default:
-		log.Fatalf("Unknown listHelperWithID interface{} %T used", i)
-	}
-
-	if err != nil {
-		return err
-	}
-
-	if opts == nil {
-		for page := 2; page <= pages; page++ {
-			if err := c.listHelperWithID(ctx, i, id, &ListOptions{PageOptions: &PageOptions{Page: page}}); err != nil {
-				return err
-			}
-		}
-	} else {
-		if opts.PageOptions == nil {
-			opts.PageOptions = &PageOptions{}
-		}
-		if opts.Page == 0 {
-			for page := 2; page <= pages; page++ {
-				opts.Page = page
-				if err := c.listHelperWithID(ctx, i, id, opts); err != nil {
-					return err
-				}
-			}
-		}
-		opts.Results = results
-		opts.Pages = pages
-	}
-
-	return nil
-}
-
-// listHelperWithTwoIDs abstracts fetching and pagination for GET endpoints that
-// require twos IDs (third level endpoints).
-// When opts (or opts.Page) is nil, all pages will be fetched and
-// returned in a single (endpoint-specific)PagedResponse
-// opts.results and opts.pages will be updated from the API response
-// nolint
-func (c *Client) listHelperWithTwoIDs(ctx context.Context, i interface{}, firstID, secondID int, opts *ListOptions) error {
-	var (
-		err     error
-		pages   int
-		results int
-		r       *resty.Response
-	)
-
-	req := c.R(ctx)
-	applyListOptionsToRequest(opts, req)
-
-	switch v := i.(type) {
-	case *NodeBalancerNodesPagedResponse:
-		if r, err = coupleAPIErrors(req.SetResult(NodeBalancerNodesPagedResponse{}).Get(v.endpointWithTwoIDs(c, firstID, secondID))); err == nil {
-			pages = r.Result().(*NodeBalancerNodesPagedResponse).Pages
-			results = r.Result().(*NodeBalancerNodesPagedResponse).Results
-			v.appendData(r.Result().(*NodeBalancerNodesPagedResponse))
-		}
-	default:
-		log.Fatalf("Unknown listHelperWithTwoIDs interface{} %T used", i)
-	}
-
-	if err != nil {
-		return err
-	}
-
-	if opts == nil {
-		for page := 2; page <= pages; page++ {
-			if err := c.listHelperWithTwoIDs(ctx, i, firstID, secondID, &ListOptions{PageOptions: &PageOptions{Page: page}}); err != nil {
-				return err
-			}
-		}
-	} else {
-		if opts.PageOptions == nil {
-			opts.PageOptions = &PageOptions{}
-		}
-		if opts.Page == 0 {
-			for page := 2; page <= pages; page++ {
-				opts.Page = page
-				if err := c.listHelperWithTwoIDs(ctx, i, firstID, secondID, opts); err != nil {
-					return err
-				}
-			}
-		}
-		opts.Results = results
-		opts.Pages = pages
-	}
-
-	return nil
-}
+/**
+case *DomainRecordsPagedResponse:
+case *FirewallDevicesPagedResponse:
+case *InstanceConfigsPagedResponse:
+case *InstanceDisksPagedResponse:
+case *InstanceVolumesPagedResponse:
+case *InvoiceItemsPagedResponse:
+case *LKEClusterAPIEndpointsPagedResponse:
+case *LKENodePoolsPagedResponse:
+case *MySQLDatabaseBackupsPagedResponse:
+case *MongoDatabaseBackupsPagedResponse:
+case *PostgresDatabaseBackupsPagedResponse:
+case *NodeBalancerConfigsPagedResponse:
+case *TaggedObjectsPagedResponse:
+    idStr := idRaw.(string)
+case TicketAttachmentsPagedResponse:
+case TicketRepliesPagedResponse:
+case *NodeBalancerNodesPagedResponse:
+    two ids
+**/
