33551e5595e3a7459bc1458f263bef4ccedc859f
Lena Garber <lgarber@linode.com>
2022-09-13 11:42:21 -0400

    fix: Retry on NGINX LB errors (#273)
    * Retry on NGINX LB errors
    
    * use HTML
    
    * go fumpt

--- test/integration/client_test.go
+++ test/integration/client_test.go
@@ -1,6 +1,10 @@
 package integration
 
 import (
+	"context"
+	"github.com/jarcoal/httpmock"
+	"github.com/linode/linodego"
+	"net/http"
 	"testing"
 )
 
@@ -24,5 +28,41 @@ func TestClient_Aliases(t *testing.T) {
 	}
 	if client.Volumes == nil {
 		t.Error("Expected alias for Volumes to return a *Resource")
+	}
+}
+
+func TestClient_NGINXRetry(t *testing.T) {
+	client := createMockClient(t)
+
+	// Recreate the NGINX LB error
+	nginxErrorFunc := func(request *http.Request) (*http.Response, error) {
+		resp := httpmock.NewStringResponse(400, "")
+
+		resp.Header.Add("Server", "nginx")
+		resp.Header.Set("Content-Type", "text/html")
+
+		return resp, nil
+	}
+
+	step := 0
+
+	httpmock.RegisterRegexpResponder("PUT",
+		mockRequestURL(t, "/profile"), func(request *http.Request) (*http.Response, error) {
+			if step == 0 {
+				step++
+				return nginxErrorFunc(request)
+			}
+
+			step++
+			return httpmock.NewJsonResponse(200, nil)
+		})
+
+	if _, err := client.UpdateProfile(context.Background(),
+		linodego.ProfileUpdateOptions{}); err != nil {
+		t.Fatal(err)
+	}
+
+	if step != 2 {
+		t.Fatalf("retry checks did not finish")
 	}
 }
