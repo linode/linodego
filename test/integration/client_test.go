445cab682860a52421a3c4945c8008d6dd94988e
Jacob Riddle <jriddle@linode.com>
2022-09-15 22:40:15 +0000

    remove depricated code

--- test/integration/client_test.go
+++ test/integration/client_test.go
@@ -2,34 +2,12 @@ package integration
 
 import (
 	"context"
-	"github.com/jarcoal/httpmock"
-	"github.com/linode/linodego"
 	"net/http"
 	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/linode/linodego"
 )
-
-func TestClient_Aliases(t *testing.T) {
-	client, _ := createTestClient(t, "")
-
-	if client.Images == nil {
-		t.Error("Expected alias for Images to return a *Resource")
-	}
-	if client.Instances == nil {
-		t.Error("Expected alias for Instances to return a *Resource")
-	}
-	if client.InstanceSnapshots == nil {
-		t.Error("Expected alias for Backups to return a *Resource")
-	}
-	if client.StackScripts == nil {
-		t.Error("Expected alias for StackScripts to return a *Resource")
-	}
-	if client.Regions == nil {
-		t.Error("Expected alias for Regions to return a *Resource")
-	}
-	if client.Volumes == nil {
-		t.Error("Expected alias for Volumes to return a *Resource")
-	}
-}
 
 func TestClient_NGINXRetry(t *testing.T) {
 	client := createMockClient(t)
