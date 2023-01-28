77ee3df425b665242dd3867449499062e297641c
Zhiwei Liang <zliang@akamai.com>
2023-01-27 23:12:53 -0600

    Undo change to coupleAPIErrors function

--- errors.go
+++ errors.go
@@ -61,7 +61,7 @@ func coupleAPIErrors(r *resty.Response, err error) (*resty.Response, error) {
 			return nil, Error{Code: http.StatusBadGateway, Message: http.StatusText(http.StatusBadGateway)}
 		}
 
-		if r.StatusCode() != 204 && responseContentType != expectedContentType {
+		if responseContentType != expectedContentType {
 			msg := fmt.Sprintf(
 				"Unexpected Content-Type: Expected: %v, Received: %v\nResponse body: %s",
 				expectedContentType,
