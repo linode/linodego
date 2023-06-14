43cf27c3347a2ce64780ba573e66eb7fb306ea23
Zhiwei Liang <zliang@akamai.com>
2023-06-14 11:12:27 -0400

    Remove unnecessary comment

--- scripts/update_fixtures_contains_specific_string.py
+++ scripts/update_fixtures_contains_specific_string.py
@@ -5,10 +5,9 @@ from pathlib import Path
 repo_dir = Path(__file__).parent.parent
 directory = repo_dir / Path("test/integration/fixtures")
 
-# search_string = "\"root_pass\":\"R34lBAdP455\""
 search_string = sys.argv[1]
-
 failed_tests = []
+
 for file in directory.glob("*"):
     if file.is_file():
         content = file.read_text()
