2a96752cc28041bccb8dd47a2f6f7b9057d5bafc
Ye Chen <yechen@akamai.com>
2023-08-11 16:27:28 -0400

    add betas

---
+++ test/integration/betas_test.go
@@ -0,0 +1,24 @@
+package integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/linode/linodego"
+)
+
+func TestBetaPrograms_List(t *testing.T) {
+	client, teardown := createTestClient(t, "fixtures/TestBetaPrograms_List")
+	defer teardown()
+
+	betas, err := client.ListBetaPrograms(context.Background(), &linodego.ListOptions{})
+	if err != nil {
+		t.Errorf("Error getting Events, expected struct, got error %v", err)
+	}
+
+	// TODO: assert data after actual data can be retrieved from API.
+	// No data is expected to be returned temporarily.
+	if len(betas) != 0 {
+		t.Errorf("Expected to see none beta program returned.")
+	}
+}
