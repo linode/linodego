d22fb85643e7c257079a7cde7c51c084f8227e5f
Ye Chen <yechen@akamai.com>
2023-08-17 10:20:59 -0400

    add test fixtures

--- test/integration/betas_test.go
+++ test/integration/betas_test.go
@@ -16,9 +16,26 @@ func TestBetaPrograms_List(t *testing.T) {
 		t.Errorf("Error getting Beta programs, expected struct, got error %v", err)
 	}
 
-	// TODO: assert data after actual data can be retrieved from API.
-	// No data is expected to be returned temporarily.
-	if len(betas) != 0 {
-		t.Errorf("Expected to see none beta program returned.")
+	if len(betas) == 0 {
+		t.Errorf("Expected to see beta program returned.")
+	} else {
+		assertDateSet(t, betas[0].Started)
 	}
+}
+
+func TestBetaProgram_Get(t *testing.T) {
+	client, teardown := createTestClient(t, "fixtures/TestBetaProgram_Get")
+	defer teardown()
+
+	betaID := "active_closed"
+	beta, err := client.GetBetaProgram(context.Background(), betaID)
+
+	if err != nil {
+		t.Errorf("Error getting Beta program, expected struct, got error %v", err)
+	}
+
+	if beta.ID != betaID {
+		t.Errorf("expected beta ID to be %s; got %s", betaID, beta.ID)
+	}
+
 }
