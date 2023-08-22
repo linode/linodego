8475f96439ea9c3a4a2b0a82cfdd6f8c17818f32
Ye Chen <127243817+yec-akamai@users.noreply.github.com>
2023-08-22 10:55:40 -0400

    new: Add `betas` to support self serve beta program (#356)
    * add betas
    
    * fix typo
    
    * revert go mod change
    
    * typo
    
    * fix linter
    
    * use started, ended
    
    * add test fixtures
    
    * fix fixtures

---
+++ test/integration/betas_test.go
@@ -0,0 +1,41 @@
+package integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/linode/linodego"
+)
+
+func TestBetaPrograms_List(t *testing.T) {
+	client, teardown := createTestClient(t, "fixtures/TestBetaPrograms_List")
+	defer teardown()
+
+	betas, err := client.ListBetaPrograms(context.Background(), &linodego.ListOptions{})
+	if err != nil {
+		t.Errorf("Error getting Beta programs, expected struct, got error %v", err)
+	}
+
+	if len(betas) == 0 {
+		t.Errorf("Expected to see beta program returned.")
+	} else {
+		assertDateSet(t, betas[0].Started)
+	}
+}
+
+func TestBetaProgram_Get(t *testing.T) {
+	client, teardown := createTestClient(t, "fixtures/TestBetaProgram_Get")
+	defer teardown()
+
+	betaID := "active_closed"
+	beta, err := client.GetBetaProgram(context.Background(), betaID)
+
+	if err != nil {
+		t.Errorf("Error getting Beta program, expected struct, got error %v", err)
+	}
+
+	if beta.ID != betaID {
+		t.Errorf("expected beta ID to be %s; got %s", betaID, beta.ID)
+	}
+
+}
