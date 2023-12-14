511984253ff68419aafd0b85c3eb1f54e436bd27
Ye Chen <127243817+yec-akamai@users.noreply.github.com>
2023-12-14 14:54:56 -0500

    new: Support `account_availability` (#434) (#444)
    * support account_availability
    
    * fix lint
    
    * fix client test
    
    * update dc to region

---
+++ test/integration/account_availability_test.go
@@ -0,0 +1,38 @@
+package integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/linode/linodego"
+)
+
+func TestAccountAvailability_List(t *testing.T) {
+	client, teardown := createTestClient(t, "fixtures/TestAccountAvailability_List")
+	defer teardown()
+
+	availabilities, err := client.ListAccountAvailabilities(context.Background(), &linodego.ListOptions{})
+	if err != nil {
+		t.Errorf("Error getting Account Availabilities, expected struct, got error %v", err)
+	}
+
+	if len(availabilities) == 0 {
+		t.Errorf("Expected to see account availabilities returned.")
+	}
+}
+
+func TestAccountAvailability_Get(t *testing.T) {
+	client, teardown := createTestClient(t, "fixtures/TestAccountAvailability_Get")
+	defer teardown()
+
+	regionID := "us-east"
+	availability, err := client.GetAccountAvailability(context.Background(), regionID)
+
+	if err != nil {
+		t.Errorf("Error getting Account Availability, expected struct, got error %v", err)
+	}
+
+	if availability.Region != regionID {
+		t.Errorf("expected region ID to be %s; got %s", regionID, availability.Region)
+	}
+}
