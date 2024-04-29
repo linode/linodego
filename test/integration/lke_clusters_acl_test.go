1bbab93e6802ad9e505bfef0fa059e448255f3af
Lena Garber <lgarber@akamai.com>
2024-04-29 13:09:03 -0400

    oops again

--- test/integration/lke_clusters_acl_test.go
+++ test/integration/lke_clusters_acl_test.go
@@ -49,7 +49,7 @@ func TestLKECluster_withACL(t *testing.T) {
 		linodego.LKEClusterControlPlaneACLUpdateOptions{
 			ACL: linodego.LKEClusterControlPlaneACL{
 				Enabled: true,
-				Addresses: linodego.LKEClusterControlPlaneACLAddresses{
+				Addresses: &linodego.LKEClusterControlPlaneACLAddresses{
 					IPv4: []string{"10.0.0.2/32"},
 					IPv6: []string{},
 				},
