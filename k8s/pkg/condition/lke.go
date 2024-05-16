2a3aedaef3cb2449a70c354d1494111e35caf103
Alex Vest <avest@akamai.com>
2024-05-16 11:36:22 +0100

    Check Linode LKE clusterID field
    This does not yet work in dev so test addition is commented out

--- k8s/pkg/condition/lke.go
+++ k8s/pkg/condition/lke.go
@@ -33,9 +33,39 @@ func ClusterHasReadyNode(ctx context.Context, options linodego.ClusterConditionO
 	return false, nil
 }
 
+// ClusterHasReadyNode is a ClusterConditionFunc which polls for at least one node to have the
+// condition NodeReady=True.
+func ClusterNodesReady(ctx context.Context, options linodego.ClusterConditionOptions) (bool, error) {
+	clientset, err := k8s.BuildClientsetFromConfig(options.LKEClusterKubeconfig, options.TransportWrapper)
+	if err != nil {
+		return false, err
+	}
+
+	nodes, err := clientset.CoreV1().Nodes().List(ctx, v1.ListOptions{})
+	if err != nil {
+		return false, fmt.Errorf("failed to get nodes for cluster: %w", err)
+	}
+
+	for _, node := range nodes.Items {
+		for _, condition := range node.Status.Conditions {
+			if condition.Type == corev1.NodeReady && condition.Status != corev1.ConditionTrue {
+				return false, nil
+			}
+		}
+	}
+	return true, nil
+}
+
 // WaitForLKEClusterReady polls with a given timeout for the LKE Cluster's api-server
 // to be healthy and for the cluster to have at least one node with the NodeReady
 // condition true.
 func WaitForLKEClusterReady(ctx context.Context, client linodego.Client, clusterID int, options linodego.LKEClusterPollOptions) error {
 	return client.WaitForLKEClusterConditions(ctx, clusterID, options, ClusterHasReadyNode)
+}
+
+// WaitForLKEClusterAndNodesReady polls with a given timeout for the LKE
+// Cluster's api-server to be healthy and for all cluster nodes to have the
+// NodeReady condition true.
+func WaitForLKEClusterAndNodesReady(ctx context.Context, client linodego.Client, clusterID int, options linodego.LKEClusterPollOptions) error {
+	return client.WaitForLKEClusterConditions(ctx, clusterID, options, ClusterNodesReady)
 }
