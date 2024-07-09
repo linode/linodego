bea68cbf477a163abd586d29dc16ee3a35e57464
vshanthe <vshanthe@akamai.com>
2024-07-09 17:50:51 +0530

    test added

---
+++ test/integration/profile_security_question_test.go
@@ -0,0 +1,20 @@
+package integration
+
+import (
+    "context"
+    "testing"
+
+    "github.com/stretchr/testify/require"
+)
+
+func TestSecurityQuestions_List(t *testing.T) {
+    client, teardown := createTestClient(t, "fixtures/TestSecurityQuestions_List")
+    defer teardown()
+
+    questions, err := client.SecurityQuestionsList(context.Background())
+    require.NoError(t, err, "Error getting security questions, expected struct")
+
+    require.NotEmpty(t, questions.SecurityQuestions, "Expected to see security questions returned")
+
+    require.Equal(t, "What was the name of your first pet?", questions.SecurityQuestions[0].Question, "Expected question 'What was the name of your first pet?'")
+}
