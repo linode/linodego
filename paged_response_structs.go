e8e27e554240b352ea55f7b9fdbe27866ee68c11
Erik Zilber <ezilber@akamai.com>
2024-07-15 12:51:11 -0400

    Migrated object_storage_keys to profile_logins from resty to request helpers (#550)
    * Migrated object_storage_keys to profile_logins and reran fixtures
    
    * Fixes
    
    * More fixes
    
    * Fixed postgres fixture

--- paged_response_structs.go
+++ paged_response_structs.go
@@ -118,6 +118,9 @@ type NotificationsPagedResponse legacyPagedResponse[Notification]
 // Deprecated: OAuthClientsPagedResponse exists for historical compatibility and should not be used.
 type OAuthClientsPagedResponse legacyPagedResponse[OAuthClient]
 
+// Deprecated: ObjectStorageKeysPagedResponse exists for historical compatibility and should not be used.
+type ObjectStorageKeysPagedResponse legacyPagedResponse[ObjectStorageKey]
+
 // Deprecated: ObjectStorageBucketsPagedResponse exists for historical compatibility and should not be used.
 type ObjectStorageBucketsPagedResponse legacyPagedResponse[ObjectStorageBucket]
 
@@ -126,6 +129,15 @@ type ObjectStorageClustersPagedResponse legacyPagedResponse[ObjectStorageCluster
 
 // Deprecated: PaymentsPagedResponse exists for historical compatibility and should not be used.
 type PaymentsPagedResponse legacyPagedResponse[Payment]
+
+// Deprecated: PostgresDatabasesPagedResponse exists for historical compatibility and should not be used.
+type PostgresDatabasesPagedResponse legacyPagedResponse[PostgresDatabase]
+
+// Deprecated: PostgresDatabaseBackupsPagedResponse exists for historical compatibility and should not be used.
+type PostgresDatabaseBackupsPagedResponse legacyPagedResponse[PostgresDatabaseBackup]
+
+// Deprecated: ProfileLoginsPagedResponse exists for historical compatibility and should not be used.
+type ProfileLoginsPagedResponse legacyPagedResponse[ProfileLogin]
 
 // Deprecated: UsersPagedResponse exists for historical compatibility and should not be used.
 type UsersPagedResponse legacyPagedResponse[User]
