c08e7903d29de4268105b111e5d570c596d2e01b
Lena Garber <lgarber@akamai.com>
2024-09-04 13:54:18 -0400

    Support base struct; add NB types endpoints

--- lke_types.go
+++ lke_types.go
@@ -8,26 +8,10 @@ import (
 // NOTE: This typically corresponds to the availability of a cluster's
 // control plane.
 type LKEType struct {
-	ID           string               `json:"id"`
-	Label        string               `json:"label"`
-	Price        LKETypePrice         `json:"price"`
-	RegionPrices []LKETypeRegionPrice `json:"region_prices"`
-	Transfer     int                  `json:"transfer"`
+	baseType
 }
 
-// LKETypePrice represents the base price for an LKE type.
-type LKETypePrice struct {
-	Hourly  float64 `json:"hourly"`
-	Monthly float64 `json:"monthly"`
-}
-
-// LKETypeRegionPrice represents the regional price for an LKE type.
-type LKETypeRegionPrice struct {
-	LKETypePrice
-	ID string `json:"id"`
-}
-
-// ListLKETypes lists linode types. This endpoint is cached by default.
+// ListLKETypes lists LKE types. This endpoint is cached by default.
 func (c *Client) ListLKETypes(ctx context.Context, opts *ListOptions) ([]LKEType, error) {
 	e := "lke/types"
 
---
+++ nodebalancer_types.go
@@ -0,0 +1,35 @@
+package linodego
+
+import (
+	"context"
+)
+
+// NodeBalancerType represents a single valid LKE type.
+// NOTE: This typically corresponds to the availability of a cluster's
+// control plane.
+type NodeBalancerType struct {
+	baseType
+}
+
+// ListNodeBalancerTypes lists NodeBalancer types. This endpoint is cached by default.
+func (c *Client) ListNodeBalancerTypes(ctx context.Context, opts *ListOptions) ([]NodeBalancerType, error) {
+	e := "nodebalancers/types"
+
+	endpoint, err := generateListCacheURL(e, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	if result := c.getCachedResponse(endpoint); result != nil {
+		return result.([]NodeBalancerType), nil
+	}
+
+	response, err := getPaginatedResults[NodeBalancerType](ctx, c, e, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	c.addCachedResponse(endpoint, response, &cacheExpiryTime)
+
+	return response, nil
+}
