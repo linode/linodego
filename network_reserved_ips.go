663e79854a232b3867fe38434ef12d0870c81658
Anirudh Jagadish <ajagadis@akamai.com>
2024-09-16 17:05:42 -0400

    Added note indicating feature is currently not available to all users

--- network_reserved_ips.go
+++ network_reserved_ips.go
@@ -5,11 +5,13 @@ import (
 )
 
 // ReserveIPOptions represents the options for reserving an IP address
+// NOTE: Reserved IP feature may not currently be available to all users.
 type ReserveIPOptions struct {
 	Region string `json:"region"`
 }
 
 // ListReservedIPAddresses retrieves a list of reserved IP addresses
+// NOTE: Reserved IP feature may not currently be available to all users.
 func (c *Client) ListReservedIPAddresses(ctx context.Context, opts *ListOptions) ([]InstanceIP, error) {
 	e := formatAPIPath("networking/reserved/ips")
 	response, err := getPaginatedResults[InstanceIP](ctx, c, e, opts)
@@ -21,6 +23,7 @@ func (c *Client) ListReservedIPAddresses(ctx context.Context, opts *ListOptions)
 }
 
 // GetReservedIPAddress retrieves details of a specific reserved IP address
+// NOTE: Reserved IP feature may not currently be available to all users.
 func (c *Client) GetReservedIPAddress(ctx context.Context, ipAddress string) (*InstanceIP, error) {
 	e := formatAPIPath("networking/reserved/ips/%s", ipAddress)
 	response, err := doGETRequest[InstanceIP](ctx, c, e)
@@ -32,6 +35,7 @@ func (c *Client) GetReservedIPAddress(ctx context.Context, ipAddress string) (*I
 }
 
 // ReserveIPAddress reserves a new IP address
+// NOTE: Reserved IP feature may not currently be available to all users.
 func (c *Client) ReserveIPAddress(ctx context.Context, opts ReserveIPOptions) (*InstanceIP, error) {
 	e := "networking/reserved/ips"
 	response, err := doPOSTRequest[InstanceIP](ctx, c, e, opts)
@@ -43,6 +47,7 @@ func (c *Client) ReserveIPAddress(ctx context.Context, opts ReserveIPOptions) (*
 }
 
 // DeleteReservedIPAddress deletes a reserved IP address
+// NOTE: Reserved IP feature may not currently be available to all users.
 func (c *Client) DeleteReservedIPAddress(ctx context.Context, ipAddress string) error {
 	e := formatAPIPath("networking/reserved/ips/%s", ipAddress)
 	return doDELETERequest(ctx, c, e)
