2efe0cfad3ef64ef53414490131edf2a5f2addb0
Ye Chen <yechen@akamai.com>
2024-11-19 10:49:23 -0500

    update domains

---
+++ test/unit/domain_test.go
@@ -0,0 +1,54 @@
+package unit
+
+import (
+	"context"
+	"github.com/linode/linodego"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDomain_Clone(t *testing.T) {
+	fixtureData, err := fixtures.GetFixture("domain_clone")
+	assert.NoError(t, err)
+
+	var base ClientBaseCase
+	base.SetUp(t)
+	defer base.TearDown(t)
+
+	requestData := linodego.DomainCloneOptions{
+		Domain: "linodego-domain-clone.com",
+	}
+
+	domainToCloneID := 123
+	base.MockPost(formatMockAPIPath("domains/%d/clone", 123), fixtureData)
+
+	domain, err := base.Client.CloneDomain(context.Background(), domainToCloneID, requestData)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, "linodego-domain-clone.com", domain.Domain)
+	assert.Equal(t, "admin@example.org", domain.SOAEmail)
+}
+
+func TestDomain_Import(t *testing.T) {
+	fixtureData, err := fixtures.GetFixture("domain_import")
+	assert.NoError(t, err)
+
+	var base ClientBaseCase
+	base.SetUp(t)
+	defer base.TearDown(t)
+
+	requestData := linodego.DomainImportOptions{
+		Domain:           "linodego-domain-import.com",
+		RemoteNameserver: "linodego-domain-import-nameserver.com",
+	}
+
+	base.MockPost("domains/import", fixtureData)
+
+	domain, err := base.Client.ImportDomain(context.Background(), requestData)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, "example.org", domain.Domain)
+	assert.Equal(t, "admin@example.org", domain.SOAEmail)
+}
