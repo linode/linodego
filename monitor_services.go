558ec7c897aaf8a2f3607e49e2e3cf2c0502f9ab
Radhika Bajoria <rbajoria@akamai.com>
2025-03-26 20:58:08 +0530

    added API for list by service_type

--- monitor_services.go
+++ monitor_services.go
@@ -14,3 +14,8 @@ type MonitorServices struct {
 func (c *Client) ListMonitorServices(ctx context.Context, opts *ListOptions) ([]MonitorServices, error) {
 	return getPaginatedResults[MonitorServices](ctx, c, "monitor/services", opts)
 }
+
+func (c *Client) GetMonitorServiceByType(ctx context.Context, service_type string, opts *ListOptions) ([]MonitorServices, error) {
+	e := formatAPIPath("monitor/services/%s", service_type)
+	return getPaginatedResults[MonitorServices](ctx, c, e, opts)
+}
--- test/unit/monitor_services_test.go
+++ test/unit/monitor_services_test.go
@@ -29,3 +29,25 @@ func TestListMonitorServices(t *testing.T) {
 	assert.Equal(t, "Databases", clients[0].Label, "Expected services label to match")
 	assert.Equal(t, "dbaas", clients[0].ServiceType, "Expected service_type to match")
 }
+
+func TestListMonitorServicesByType(t *testing.T) {
+	// Load the mock fixture for Longview clients
+	fixtureData, err := fixtures.GetFixture("monitor_services")
+	assert.NoError(t, err, "Expected no error when getting fixture")
+
+	var base ClientBaseCase
+	base.SetUp(t)
+	defer base.TearDown(t)
+
+	// Mock the GET request for the longview clients endpoint
+	base.MockGet("monitor/services/dbaas", fixtureData)
+
+	// Call the ListMonitorServices method
+	clients, err := base.Client.GetMonitorServiceByType(context.Background(), "dbaas", &linodego.ListOptions{})
+	assert.NoError(t, err, "Expected no error when listing monitor services")
+	assert.NotEmpty(t, clients, "Expected non-empty monitor services list")
+
+	// Validate the first longview client details
+	assert.Equal(t, "Databases", clients[0].Label, "Expected services label to match")
+	assert.Equal(t, "dbaas", clients[0].ServiceType, "Expected service_type to match")
+}
