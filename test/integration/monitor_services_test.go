240755435e7a633f72948b280430e343a16fbfcc
Radhika Bajoria <rbajoria@akamai.com>
2025-03-26 20:58:08 +0530

    added API for list by service_type
    profile_tests (#698)

--- test/integration/monitor_services_test.go
+++ test/integration/monitor_services_test.go
@@ -2,6 +2,7 @@ package integration
 
 import (
 	"context"
+	"log"
 	"testing"
 
 	"github.com/linode/linodego"
@@ -12,15 +13,35 @@ func TestMonitorServices_Get_smoke(t *testing.T) {
 	client, teardown := createTestClient(t, "fixtures/TestMonitorServices_Get")
 	defer teardown()
 
-	MonitorServicesClientList, listErr := client.ListMonitorServices(context.Background(), nil)
+	monitorServicesClientList, listErr := client.ListMonitorServices(context.Background(), nil)
 	if listErr != nil {
 		t.Errorf("Error listing monitor services:%s", listErr)
 	}
 
 	// found := false
-	for _, services := range MonitorServicesClientList {
+	for _, services := range monitorServicesClientList {
 		validateServiceTypes(t, services)
 	}
+
+	monitorServicesClient, getErr := client.GetMonitorServiceByType(context.Background(), "dbaas", nil)
+	if getErr != nil {
+		t.Errorf("Error getting monitor services : %s", getErr)
+	}
+
+	found := false
+	for _, element := range monitorServicesClient {
+		log.Printf("[INFO] element.ServiceType : %#+v", element.ServiceType)
+		log.Printf("[INFO] monitorServicesClient.ServiceType : %#+v", monitorServicesClient)
+		if element.ServiceType == "dbaas" {
+			log.Printf("[WARN] event.Created is nil when API returned: %#+v", element.ServiceType)
+			found = true
+		}
+	}
+
+	if !found {
+		t.Errorf("Monitor service not found in list.")
+	}
+
 }
 
 func validateServiceTypes(
