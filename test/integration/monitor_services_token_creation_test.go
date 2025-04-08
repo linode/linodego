aa23897ffde04357e2ebc9ddedbe86d50b4d548b
Radhika Bajoria <rbajoria@akamai.com>
2025-04-08 20:55:41 +0530

    updated integration test for token creation

--- test/integration/monitor_services_token_creation_test.go
+++ test/integration/monitor_services_token_creation_test.go
@@ -9,15 +9,36 @@ import (
 )
 
 func TestMonitorServicesTokenCreation_Get_smoke(t *testing.T) {
-	client, teardown := createTestClient(t, "fixtures/TestMonitorServiceToken_POST")
+
+	client, _, teardown, err := setupPostgresDatabase(t, nil, "fixtures/TestDatabase_List")
+	if err != nil {
+		t.Error(err)
+	}
 	defer teardown()
+
+	dbs, err := client.ListDatabases(context.Background(), nil)
+	if len(dbs) == 0 {
+		t.Errorf("Expected a list of Databases, but got none: %v", err)
+	}
+	if err != nil {
+		t.Errorf("Error listing Databases, expected struct, got error %v", err)
+	}
+
+	// Extract IDs from dbs
+	var entityIDs []int
+	for _, db := range dbs {
+		entityIDs = append(entityIDs, db.ID)
+	}
+
+	client1, teardown1 := createTestClient(t, "fixtures/TestMonitorServiceToken_POST")
+	defer teardown1()
 
 	// Create a JWE token for the given entity IDs
 	createOpts := linodego.MonitorTokenCreateOptions{
-		EntityIDs: []int{12345, 54321},
+		EntityIDs: entityIDs,
 	}
 
-	token, getErr := client.CreateMonitorServiceTokenForServiceType(context.Background(), "dbaas", createOpts)
+	token, getErr := client1.CreateMonitorServiceTokenForServiceType(context.Background(), "dbaas", createOpts)
 	if getErr != nil {
 		t.Errorf("Error creating token : %s", getErr)
 	}
