94cb953bd6837aa78792c37e780e2b2ca7923eec
Jacob Riddle <87780794+jriddle-linode@users.noreply.github.com>
2025-06-03 10:51:53 -0400

    Support for Entities and IAM endpoints (#754)
    ## 📝 Description
    
    **What does this PR do and why is this change necessary?**
    
    Implements:
    - **GET** /entities
    - **GET** /iam/role-permissions
    - **GET** /iam/users/{username}/role-permissions
    - **PUT** /iam/users/{username}/role-permissions
    
    ## ✔️ How to Test
    
    **How do I run the relevant unit/integration tests?**
    
    ```bash
    make TEST_ARGS='-run TestIAM' test-unit
    ```
    ```bash
    make TEST_ARGS='-run TestEntities' test-unit
    ```

---
+++ test/unit/iam_test.go
@@ -0,0 +1,63 @@
+package unit
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIAMAccountRolerPermissions_Get(t *testing.T) {
+	fixtureData, err := fixtures.GetFixture("iam_account_get")
+	assert.NoError(t, err)
+
+	var base ClientBaseCase
+	base.SetUp(t)
+	defer base.TearDown(t)
+
+	base.MockGet(formatMockAPIPath("iam/role-permissions"), fixtureData)
+
+	perms, err := base.Client.GetAccountRolePermissions(context.Background())
+	assert.NoError(t, err)
+
+	assert.Equal(t, "linode", perms.EntityAccess[0].Type)
+}
+
+func TestIAMUserRolePermissions_Get(t *testing.T) {
+	fixtureData, err := fixtures.GetFixture("iam_user_get")
+	assert.NoError(t, err)
+
+	var base ClientBaseCase
+	base.SetUp(t)
+	defer base.TearDown(t)
+
+	base.MockGet(formatMockAPIPath("iam/users/Linode/role-permissions"), fixtureData)
+
+	perms, err := base.Client.GetUserRolePermissions(context.Background(), "Linode")
+	assert.NoError(t, err)
+
+	assert.Equal(t, 1, perms.EntityAccess[0].ID)
+}
+
+func TestIAMUserRolePermissions_Update(t *testing.T) {
+	updateFixtureData, err := fixtures.GetFixture("iam_user_update")
+	assert.NoError(t, err)
+	getFixtureData, err := fixtures.GetFixture("iam_user_get")
+	assert.NoError(t, err)
+
+	var base ClientBaseCase
+	base.SetUp(t)
+	defer base.TearDown(t)
+
+	base.MockGet(formatMockAPIPath("iam/users/Linode/role-permissions"), getFixtureData)
+	base.MockPut(formatMockAPIPath("iam/users/Linode/role-permissions"), updateFixtureData)
+
+	before, err := base.Client.GetUserRolePermissions(context.Background(), "Linode")
+	opts := before.GetUpdateOptions()
+	opts.AccountAccess = append(opts.AccountAccess, "test_admin")
+	after, err := base.Client.UpdateUserRolePermissions(context.Background(), "Linode", opts)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 1, after.EntityAccess[0].ID)
+	assert.NotEqual(t, before.AccountAccess, after.AccountAccess)
+}
