663223542dcb39a072a38831e2e019a49c54401b
ezilber-akamai <ezilber@akamai.com>
2025-08-13 11:33:46 -0400

    Fixed account settings tests

--- test/integration/account_settings_test.go
+++ test/integration/account_settings_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestAccountSettings_Get(t *testing.T) {
-	client, teardown := createTestClient(t, "fixtures/TestAccountSettings")
+	client, teardown := createTestClient(t, "fixtures/TestAccountSettings_Get")
 	defer teardown()
 
 	// Mocking the API response
@@ -22,7 +22,7 @@ func TestAccountSettings_Get(t *testing.T) {
 		BackupsEnabled:       true,
 		Managed:              true,
 		NetworkHelper:        true,
-		LongviewSubscription: String("longview-3"),
+		LongviewSubscription: String("longview-100"),
 		ObjectStorage:        String("active"),
 	}
 	mockResponse, _ := json.Marshal(mockSettings)
@@ -37,13 +37,13 @@ func TestAccountSettings_Get(t *testing.T) {
 	require.True(t, settings.Managed, "Expected Managed to be true")
 	require.True(t, settings.NetworkHelper, "Expected NetworkHelper to be true")
 	require.NotNil(t, settings.LongviewSubscription, "Expected LongviewSubscription to be non-nil")
-	require.Equal(t, "longview-3", *settings.LongviewSubscription, "Expected LongviewSubscription to be 'longview-3'")
+	require.Equal(t, "longview-100", *settings.LongviewSubscription, "Expected LongviewSubscription to be 'longview-100'")
 	require.NotNil(t, settings.ObjectStorage, "Expected ObjectStorage to be non-nil")
 	require.Equal(t, "active", *settings.ObjectStorage, "Expected ObjectStorage to be 'active'")
 }
 
 func TestAccountSettings_Update(t *testing.T) {
-	client, teardown := createTestClient(t, "fixtures/TestAccountSettings")
+	client, teardown := createTestClient(t, "fixtures/TestAccountSettings_Update")
 	defer teardown()
 
 	// Mocking the API response
@@ -51,15 +51,14 @@ func TestAccountSettings_Update(t *testing.T) {
 	defer httpmock.DeactivateAndReset()
 
 	opts := linodego.AccountSettingsUpdateOptions{
-		BackupsEnabled:       Bool(false),
-		LongviewSubscription: String("longview-10"),
-		NetworkHelper:        Bool(false),
+		BackupsEnabled: Bool(false),
+		NetworkHelper:  Bool(false),
 	}
 
 	mockSettings := linodego.AccountSettings{
 		BackupsEnabled:       false,
 		NetworkHelper:        false,
-		LongviewSubscription: String("longview-10"),
+		LongviewSubscription: String("longview-100"),
 	}
 	mockResponse, _ := json.Marshal(mockSettings)
 
@@ -72,7 +71,7 @@ func TestAccountSettings_Update(t *testing.T) {
 	require.False(t, settings.BackupsEnabled, "Expected BackupsEnabled to be false")
 	require.False(t, settings.NetworkHelper, "Expected NetworkHelper to be false")
 	require.NotNil(t, settings.LongviewSubscription, "Expected LongviewSubscription to be non-nil")
-	require.Equal(t, "longview-10", *settings.LongviewSubscription, "Expected LongviewSubscription to be 'longview-10'")
+	require.Equal(t, "longview-100", *settings.LongviewSubscription, "Expected LongviewSubscription to be 'longview-100'")
 }
 
 func Bool(v bool) *bool       { return &v }
