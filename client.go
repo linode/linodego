208f1492b53e5f1a1d0c4647a4803fb240d9a18f
Zhiwei Liang <zliang@akamai.com>
2026-04-21 20:11:27 -0400

    redact request header log

--- client.go
+++ client.go
@@ -696,7 +696,7 @@ func (c *Client) logRequest(req *http.Request) *http.Request {
 	reqLog := &RequestLog{
 		Request: strings.Join([]string{req.Method, req.URL.Path, req.Proto}, " "),
 		Host:    req.Host,
-		Headers: req.Header.Clone(),
+		Headers: redactHeaders(req.Header.Clone()),
 		Body:    reqBody.String(),
 	}
 
