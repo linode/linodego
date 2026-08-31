fd6b6494bde9e325f993abf20374a89f521b31a1
avmishra <avmishra@akamai.com>
2026-08-31 13:04:22 -0400

    adding flexible pg limit into the RegionPlacementGroupLimits struct

--- regions.go
+++ regions.go
@@ -100,8 +100,9 @@ type RegionMonitors struct {
 // RegionPlacementGroupLimits contains information about the
 // placement group limits for the current user in the current region.
 type RegionPlacementGroupLimits struct {
-	MaximumPGsPerCustomer int `json:"maximum_pgs_per_customer"`
-	MaximumLinodesPerPG   int `json:"maximum_linodes_per_pg"`
+	MaximumPGsPerCustomer       int `json:"maximum_pgs_per_customer"`
+	MaximumLinodesPerPG         int `json:"maximum_linodes_per_pg"`
+	MaximumLinodesPerFlexiblePG int `json:"maximum_linodes_per_flexible_pg"`
 }
 
 // ListRegions lists Regions. This endpoint is cached by default.
